7bf57bb5421d9a7eca112d4823ab29107e5dc2cc
Pxxjan <poojansoni595@gmail.com>
2024-02-20 15:11:13 +0530

    store application in database

--- database.py
+++ database.py
@@ -34,3 +34,22 @@ def load_job_from_db(id):
         return job_dict
     else:
         return None
+    
+app_table = 'applications'
+applications = metadata.tables[app_table]
+    
+def add_application_to_db(job_id, data):
+
+    insert_statement = applications.insert().values(
+        job_id=job_id,
+        full_name=data['full_name'],
+        email=data['email'],
+        linkedin_url=data['linkedin_url'],
+        education=data['education'],
+        work_experience=data['work_experience'],
+        resume_url=data['resume_url']
+    )
+
+    with engine.connect() as conn:
+        conn.execute(insert_statement)
+        conn.commit()
--- sqlite/sql.sql
+++ sqlite/sql.sql
@@ -1,9 +1,22 @@
 -- INSERT INTO jobs(title, location, salary)
 -- VALUES ('Backend Engineer', 'San Francisco, USA', '150,000');
 
-UPDATE jobs
-SET responsibilities= 'Expertise in architect, developing, and maintaining the core functionality and databases of web applications. Your responsibilities include optimizing performance, ensuring scalability, implementing security measures, and integrating with third-party services. Collaborate with frontend developers to deliver seamless user experiences and troubleshoot issues for robust backend solutions.'
-WHERE id = 4;
+-- UPDATE jobs
+-- SET responsibilities= 'Expertise in architect, developing, and maintaining the core functionality and databases of web applications. Your responsibilities include optimizing performance, ensuring scalability, implementing security measures, and integrating with third-party services. Collaborate with frontend developers to deliver seamless user experiences and troubleshoot issues for robust backend solutions.'
+-- WHERE id = 4;
 
 -- INSERT INTO jobs(currency, responsibilities, requirements)
 -- VALUES ('Rs.', '', '');
+
+-- CREATE TABLE applications ( 
+-- id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+-- job_id INTEGER NOT NULL,
+-- full_name VARCHAR(250) NOT NULL,
+-- email VARCHAR(250) NOT NULL,
+-- linkedin_url VARCHAR(500),
+-- education VARCHAR(2000),
+-- work_experience VARCHAR(2000),
+-- resume_url VARCHAR(500)
+-- );
+
+-- DROP TABLE applications;
