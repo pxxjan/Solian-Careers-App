117158a05556c32218dce917bc5eb0cd466b1a9d
Pxxjan <poojansoni595@gmail.com>
2024-02-20 16:36:41 +0530

    validated form

--- temp.py
+++ temp.py
@@ -1,18 +1,18 @@
-from sqlalchemy import create_engine, MetaData
+# from sqlalchemy import create_engine, MetaData
 
-# Define the path to your SQLite database
-db_path = r'C:\Users\Lenovo\OneDrive\Desktop\Flask App\sqlite\solian.db'
+# # Define the path to your SQLite database
+# db_path = r'C:\Users\Lenovo\OneDrive\Desktop\Flask App\sqlite\solian.db'
 
-# Create an engine to connect to the database
-engine = create_engine(f'sqlite:///{db_path}', echo=True)
+# # Create an engine to connect to the database
+# engine = create_engine(f'sqlite:///{db_path}', echo=True)
 
-# Initialize a new Metadata object
-metadata = MetaData()
+# # Initialize a new Metadata object
+# metadata = MetaData()
 
-# Reflect tables from the database
-metadata.reflect(bind=engine)
+# # Reflect tables from the database
+# metadata.reflect(bind=engine)
 
-# Print out the list of table names reflected from the database
-print("Tables reflected from the database:")
-for table_name in metadata.tables.keys():
-    print(table_name)
+# # Print out the list of table names reflected from the database
+# print("Tables reflected from the database:")
+# for table_name in metadata.tables.keys():
+#     print(table_name)
